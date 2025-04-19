4971656f53236059b2e9992f73e22681df09a063
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-04-20 00:47:41 +0900

    エディションを 2024 に変更した。

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,9 +1,9 @@
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub const extern "C" fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub const extern "C" fn sub(a: i32, b: i32) -> i32 {
     a - b
 }
